89042a870c1546199578cb3108395e278047fc44
enzogbiya <lipnitskiy.kirill@mail.ru>
2022-09-27 22:42:14 +0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+let api_url = "https://api.hh.ru/vacancies";
+
+let request_params = {
+	text: "",
+	area: [1002, 1004, 1003, 1006, 1007],
+	per_page: 100,
+	period: 7,
+	host: "rabota.by",
+	order_by: "publication_time",
+};
+
+const parserModule = angular.module("parser", []);
+
+parserModule.controller("BodyController", function ($scope, $http) {
+	$scope.searchVacancies = function (string) {
+		console.log(string);
+		request_params.text = string;
+		$http({
+			method: "GET",
+			url: api_url,
+			params: request_params,
+		}).then(
+			function (response) {
+				$scope.items = response.data.items;
+				console.log(response);
+				console.log(response.data.items);
+			},
+			function (error) {}
+		);
+	};
+});
